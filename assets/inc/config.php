7412a7f5fef69feddaf3dd650cc39df1e7c589a4
Abdelsalam Dogorshom <saloom.dogorshom@gmail.com>
2024-08-06 16:21:41 +0300

    fix config

--- assets/inc/config.php
+++ assets/inc/config.php
@@ -1,7 +1,7 @@
 <?php
-$dbuser="root";
-$dbpass="";
-$host="localhost";
-$db="orrsphp";
+$dbuser=$_ENV['DATABASE_USER'];
+$dbpass=$_ENV['DATABASE_PASS'];
+$host=$_ENV['DATABASE_HOST'];
+$db=$_ENV['DATABASE_NAME'];
 $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
 ?>
